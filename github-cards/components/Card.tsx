ec6fd3c860b77ad51174fa8018d44fd083a7bcc5
katf101 <vawav11@gmail.com>
2022-07-15 11:29:16 +0900

    Card 컴포넌트 css 완성

--- github-cards/components/Card.tsx
+++ github-cards/components/Card.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { profileType } from "../utils";
+import styles from "./Card.module.scss";
 
 interface CardPropType {
   profile: profileType;
@@ -7,9 +8,26 @@ interface CardPropType {
 
 const Card = ({ profile }: CardPropType) => {
   return (
-    <>
-      <h1>Card</h1>
-    </>
+    <div className={styles.container}>
+      <div>
+        <img src={profile.avatar_url} alt="Avatar" />
+      </div>
+      <div>
+        <h2>
+          {profile.name} ({profile.login})
+        </h2>
+        <p>{profile.bio}</p>
+        <p>
+          {profile.blog && (
+            <a href={profile.blog} target="_blank" rel="noopener noreferrer">
+              Website
+            </a>
+          )}
+          <code>Public Repositories: {profile.public_repos}</code>
+          <code>Public Gits: {profile.public_gists}</code>
+        </p>
+      </div>
+    </div>
   );
 };
 
--- github-cards/utils/index.ts
+++ github-cards/utils/index.ts
@@ -10,12 +10,12 @@ export type profileType = {
 
 export const defaultProfiles: profileType[] = [
   {
-    name: "Mayank",
-    login: "mayank1513",
-    avatar_url: "https://avatars.githubusercontent.com/u/25501269?v=4",
+    name: "Hansol",
+    login: "katf101",
+    avatar_url: "https://avatars.githubusercontent.com/u/102638663?v=4",
     blog: "https://mayank-chaudhari.web.app",
-    bio: "Passionate Developer and Researcher. Coming from academic and research background, and actively involved in designing simulation tools and app development.",
-    public_repos: 62,
-    public_gists: 19,
+    bio: "Javascript 정복하기",
+    public_repos: 17,
+    public_gists: 1,
   },
 ];
